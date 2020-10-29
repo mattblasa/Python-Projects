62842537a945a41d93c20de7e9b53f9b4b5e00d5
Matthew Blasa <55324935+mattblasa@users.noreply.github.com>
2020-10-29 18:26:30 -0500

    Delete Category.py

--- Student Report Card/Category.py
+++
@@ -1,7 +0,0 @@
-class Category:
-    def _init_(self, categoryName):
-        self.name = categoryName
-        self.weight = 0
-    
-    def setWeight(self, percentage):
-        self.weight = int(percentage)
